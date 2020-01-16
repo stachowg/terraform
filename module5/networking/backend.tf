c305317d81e97b0f0da1eee0195ca7a4ac681bda
stachowg <56914699+stachowg@users.noreply.github.com>
2020-01-16 12:25:50 +0100

    Update backend.tf

--- module5/networking/backend.tf
+++ module5/networking/backend.tf
@@ -4,7 +4,7 @@
 terraform {
   backend "s3" {
     key            = "networking.state"
-    region         = "us-west-2"
-    dynamodb_table = "ddt-tfstatelock"
+    region         = "us-east-2"
+    dynamodb_table = "ddttfstatelock"
   }
 }
