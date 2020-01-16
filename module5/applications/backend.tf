3b20da5778590bfd5888ef75897c6bca323dbb4f
stachowg <56914699+stachowg@users.noreply.github.com>
2020-01-16 14:01:54 +0100

    Update backend.tf

--- module5/applications/backend.tf
+++ module5/applications/backend.tf
@@ -4,7 +4,7 @@
 terraform {
   backend "s3" {
     key            = "application.state"
-    region         = "us-west-2"
-    dynamodb_table = "ddt-tfstatelock"
+    region         = "us-east-2"
+    dynamodb_table = "ddttfstatelock"
   }
 }
