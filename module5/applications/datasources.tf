3bb6e4dd9449dff6bb31fdef4aca2b0c360f1153
stachowg <56914699+stachowg@users.noreply.github.com>
2020-01-16 14:10:29 +0100

    Update datasources.tf

--- module5/applications/datasources.tf
+++ module5/applications/datasources.tf
@@ -24,7 +24,8 @@ data "terraform_remote_state" "networking" {
 
 data "aws_ami" "ubuntu" {
   most_recent = true
-
+  owners = ["self"] 
+  
   filter {
     name   = "name"
     values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server*"]
